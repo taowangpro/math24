63d362793a119ccd474ba77d58fb2afeb78a94a6
Tao Wang <t_wang@jea.ca>
2018-11-13 05:04:04 +0000

    init setup

---
+++ index.js
@@ -0,0 +1,8 @@
+import path from 'path';
+
+const log = string => {
+  console.log('Hello');
+}
+
+log('Hello World!');
+
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: './bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader'
+    }]
+  }
+};
