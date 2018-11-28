ce6c4e271fe9d0312a7195f2311ab1700557dfc5
Tao Wang <t_wang@jea.ca>
2018-11-28 17:58:59 +0000

    all in

--- index.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-
-const log = string => {
-  console.log('Hello');
-}
-
-log('Hello World!');
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,46 @@
+const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './index.js',
-  output: {
-    path: __dirname,
-    filename: './bundle.js'
-  },
-  module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader'
-    }]
-  }
+const config = (env, argv) => {
+  return {
+    mode: argv.mode !== 'production' ? 'development' : 'production',
+    devtool: argv.mode !== 'production' ? 'source-map' : undefined,
+
+    context: path.resolve(__dirname, 'src'),
+
+    entry: {
+      main: [
+//        'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+        './index.js'
+      ]
+    },
+
+    output: {
+      path: __dirname,
+      filename: './dist/bundle.js'
+    },
+
+    resolve: {
+      extensions: ['.js', '.json', '.jsx']
+    },
+
+    module: {
+      rules: [{
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      }]
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        inject: 'body'
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin()
+    ]
+  };
 };
+
+module.exports = config;
